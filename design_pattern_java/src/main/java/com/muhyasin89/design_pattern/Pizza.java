029348aa5ea1bd4d1b6a3583e47422e50ceddbfe
muhyasin89 <muhyasin89@gmail.com>
2020-07-16 10:57:35 +0700

    [INIT]decorator pattern

---
+++ design_pattern_java/src/main/java/com/muhyasin89/design_pattern/Pizza.java
@@ -0,0 +1,10 @@
+public class Pizza {
+    
+    public abstract void setDescription(String newDescription);
+
+    public abstract String getDescription();
+
+    public abstract double getCost();
+
+    public abstract boolean hasFontina();
+}
