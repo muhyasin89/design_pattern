28788392e15a16a0c60805936ecb04ebf728635d
Muhammad Yasin <muhammadyasin@polyglotengineer.local>
2021-09-13 12:15:00 +0700

    [master]add factory java

---
+++ Structural/Decorator/Java/ThreeCheesePizza.java
@@ -0,0 +1,17 @@
+public class ThreeCheesePizza {
+    @Override
+    public Void setDescription(String newDescription){
+
+    }
+
+    @Override
+    public String getDescription(){
+        return "Mozzarella, Fontina, Parmesan, Cheese Pizza";
+        
+    }
+
+    @Override
+    public double getCost(){
+        return 10.00;
+    }
+}
